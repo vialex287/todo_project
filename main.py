0c052a425be38c4c683994a54c7b37759673d748
vivivi <vialex287@gmail.com>
2025-07-10 16:15:20 +0200

    Added main.py, requirements

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import uvicorn
+import asyncio
+from app.core.database import Base, engine
+from app.api.routers import router_tasks, router_users
+
+app = FastAPI()
+
+# create tables
+async def create_tables():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+    print("Таблицы успешно созданы в базе данных!")
+
+
+@app.on_event("startup")
+async def startup_event():
+    await create_tables()
+
+
+#view headers
+app.include_router(router_users)
+app.include_router(router_tasks)
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True)
