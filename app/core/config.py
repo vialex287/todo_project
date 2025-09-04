c8081453f330f408396423386acd93126e948c63
vivivi <vialex287@gmail.com>
2025-09-04 16:06:22 +0200

    2. Fixing config

--- app/core/config.py
+++ app/core/config.py
@@ -20,15 +20,19 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
-        env_file = ".env"
+        env_file = ".env.example"
+
 
     @property
     def DATABASE_URL(self):
-        return (
-            f"{self.DB_TYPE}://{self.DB_USER}:{self.DB_PASSWORD}@"
-            f"{self.DB_HOST}/{self.DB_NAME}"
-            f"?charset=utf8mb4"
-        )
+        if self.DB_TYPE.startswith("sqlite"):
+            return f"{settings.DB_TYPE}:///{settings.DB_NAME}"
+        else:
+            return (
+                f"{self.DB_TYPE}://{self.DB_USER}:{self.DB_PASSWORD}@"
+                f"{self.DB_HOST}/{self.DB_NAME}"
+                f"?charset=utf8mb4"
+            )
 
 
 settings = Settings()
